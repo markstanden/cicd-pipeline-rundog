83c6115f5e453f0a6eee4c5059eb6df89d10a8e1
Mark Standen <no-reply@rundog.dev>
2025-08-17 20:11:07 +0100

    Makes test fail!

--- rundog.E2E.Tests/Pages/HomeTests.cs
+++ rundog.E2E.Tests/Pages/HomeTests.cs
@@ -1,18 +1,18 @@
 using System.Text.RegularExpressions;
 using Microsoft.Playwright.Xunit;
 
-namespace Pages;
+namespace rundog.E2E.Tests.Pages;
 
-public class HomeTests: PageTest
+public class HomeTests : PageTest
 {
     [Fact]
     public async Task HasTitle()
     {
         // Arrange
-        const string expectedSubstring = "Playwright";
-        Regex expectedTitle = new Regex(expectedSubstring);
+        const string expectedSubstring = "FAILINGTEST";
+        Regex expectedTitle = new(expectedSubstring);
         const string pageUrl = "https://playwright.dev";
-        
+
         // Act
         await Page.GotoAsync(pageUrl);
 
