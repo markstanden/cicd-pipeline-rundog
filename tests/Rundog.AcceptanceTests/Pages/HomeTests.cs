b6a152b7548f307ee5c89b8fb9bb03513f2de57d
Mark Standen <no-reply@rundog.dev>
2025-08-21 21:55:07 +0100

    Extracts page layout into components, removes bootstrap classes, adds testid handles

--- tests/Rundog.AcceptanceTests/Pages/HomeTests.cs
+++ tests/Rundog.AcceptanceTests/Pages/HomeTests.cs
@@ -1,5 +1,6 @@
-using System.Text.RegularExpressions;
+using Microsoft.Playwright;
 using Microsoft.Playwright.Xunit;
+using Rundog.Core.Constants;
 using Rundog.Core.EnvironmentVariables;
 
 namespace Rundog.AcceptanceTests.Pages;
@@ -7,18 +8,33 @@ namespace Rundog.AcceptanceTests.Pages;
 [Trait("Environment", "Production")]
 public class HomeTests : PageTest
 {
+    private readonly string _pageUrl = BaseUrl.Value;
+
     [Fact]
-    public async Task HasTitle()
+    public async Task HasExpectedTitle()
     {
         // Arrange
-        const string expectedSubstring = "Home";
-        Regex expectedTitle = new(expectedSubstring);
-        string pageUrl = BaseUrl.Value;
+        const string expectedTitle = Site.Title;
 
         // Act
-        await Page.GotoAsync(pageUrl);
+        await Page.GotoAsync(_pageUrl);
 
         // Assert
         await Expect(Page).ToHaveTitleAsync(expectedTitle);
+    }
+
+    [Fact]
+    public async Task HasExpectedSlogan()
+    {
+        // Arrange
+        const string testId = "site-slogan";
+        const string expectedText = Site.Slogan;
+
+        // Act
+        await Page.GotoAsync(_pageUrl);
+        ILocator sloganElement = Page.GetByTestId(testId);
+
+        // Assert
+        await Expect(sloganElement).ToHaveTextAsync(expectedText);
     }
 }
