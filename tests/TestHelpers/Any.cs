f1a6fa8a1c7a66e99d07c6362e4b89ea13063bc5
Mark Standen <no-reply@rundog.dev>
2025-09-25 07:23:34 +0100

    Adds TestHelpers and Core unit test projects

---
+++ tests/TestHelpers/Any.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace TestHelpers;
+
+public static class Any
+{
+    // Strings
+    public const string String = "Any String";
+    public const string FourCharString = "abcd";
+    public const string FiveCharString = "abcde";
+    public const string SixCharString = "abcdef";
+    public const string ShortAlphanumericString = "ABC12345";
+    public const string LongAlphanumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    public const string ShortAlphaWithSpecialCharsString = "$A^B*C(D)&1<2>3?4%";
+    public const string LongAlphaWithSpecialCharsString = "$-A^B*C(D)&*£!<>|,.?;:@'~#[]{}-_-+1=`3¬%";
+    public const string UrlString = "https://www.anydomain.com";
+    public const string OtherUrlString = "https://www.anotherdomain.com";
+
+    // Numbers
+    public const int Integer = 1;
+
+    // URIs
+    public static readonly Uri Url = new(UrlString);
+    public static readonly Uri OtherUrl = new(OtherUrlString);
+
+    // Dates
+    public static readonly DateTime UtcDate = new(2025, 8, 31, 0, 0, 0, DateTimeKind.Utc);
+    public static readonly DateTime UtcLeapYear = new(2024, 2, 29, 0, 0, 0, DateTimeKind.Utc);
+}
