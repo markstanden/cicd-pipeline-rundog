a28bb107dfd4f3abf25593a4d03c6a28c3763af2
Mark Standen <no-reply@rundog.dev>
2025-09-27 21:28:37 +0100

    Add name, slogan, and introduction to site constants and display on Home page

--- src/Core/Constants/Site.cs
+++ src/Core/Constants/Site.cs
@@ -12,7 +12,18 @@ public static class Site
     public const string Title = "rundog.dev";
 
     /// <summary>
+    ///     The name of the developer described on the page.
+    /// </summary>
+    public const string Name = "Mark Standen";
+
+    /// <summary>
     ///     The catchy slogan that embodies the site
     /// </summary>
-    public const string Slogan = "Think twice, code once.";
+    public const string Slogan ="Think twice, code once.";
+
+    /// <summary>
+    ///     A brief introduction describing the expertise and areas of focus.
+    /// </summary>
+    public const string Introduction =
+        "Azure Developer specialising in C# microservices, TDD practices, and automated CI/CD pipelines.";
 }
