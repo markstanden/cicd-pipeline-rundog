d44e4ccc53f92132a57ba2711084363aac6b1613
Mark Standen <no-reply@rundog.dev>
2025-09-28 10:13:27 +0100

    Adds TestIds static class for sharing of test handles between projects

---
+++ src/Core/Constants/TestIds.cs
@@ -0,0 +1,40 @@
+namespace Rundog.Core.Constants;
+
+/// <summary>
+///     A collection of constant test handles used throughout the application for testing purposes.
+/// </summary>
+public static class TestIds
+{
+    /// <summary>
+    ///     Site global elements
+    /// </summary>
+    public static class Site
+    {
+        /// <summary>
+        ///     The main title of the Site.
+        ///     Displayed in the tab within the browser
+        /// </summary>
+        public const string Title = "site-title";
+    }
+
+    /// <summary>
+    ///     Test handles for elements within the developer Hero
+    /// </summary>
+    public static class Hero
+    {
+        /// <summary>
+        /// The identifier for the developer's name used in testing.
+        /// </summary>
+        public const string Name = "hero-name";
+
+        /// <summary>
+        /// The identifier for the developer's slogan used in testing.
+        /// </summary>
+        public const string Slogan = "hero-slogan";
+
+        /// <summary>
+        /// The identifier for the developer's introduction or brief description used in testing.
+        /// </summary>
+        public const string Intro = "hero-intro";
+    }
+}
